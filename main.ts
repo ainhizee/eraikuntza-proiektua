116fe2289a821c4539328ad3b0bcf6c7f7880755
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-03-11 14:10:03 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Puntuazioa = 0
+})
+let Puntuazioa = 0
+Puntuazioa = 0
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P0)) {
-        game.addScore(1)
-        basic.showNumber(game.score())
+        Puntuazioa += 1
+        basic.showNumber(Puntuazioa)
+        music.playMelody("C5 - - - - - - - ", 120)
+    }
+    if (Puntuazioa == 25) {
+        basic.showString("irabazi duzu :)")
     }
 })
